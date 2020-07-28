9398a38576be67515a5af0478bda292aab8acc0f
Roberto Ribas <rrcastaway@gmail.com>
2020-07-28 18:13:20 +0100

    now it can read a bufr file from the command line and print the number of messages

--- read_bufr.py
+++ read_bufr.py
@@ -1,7 +1,15 @@
 from eccodes import *
-
+import sys 
 def main():
     print(codes_get_api_version())
+    if ( len( sys.argv) != 2):
+        print(f" usage {sys.argv[0]} bufr file")
+    else:
+        fname=sys.argv[1]
+        f=open(fname,"rb")
+        nmsg=codes_count_in_file(f)
+        print(f"number of message {nmsg}")
+        f.close()
 
 
 if __name__=="__main__":
